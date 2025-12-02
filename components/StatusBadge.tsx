282d342c8d36f469f4d0dbc0dbb92f1b471dae85
Afolayan Joshua <afolayanjoshua36@gmail.com>
2025-12-02 17:03:07 +0100

    Frontend commits

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -9,7 +9,7 @@ const StatusBadge = ({ status } : {status: TaskStatus}) => {
 
   const config = statusConfig[status];
   return (
-     <div className="flex items-center gap-2">
+     <div className="flex items-center flex gap-2">
       <div className={`size-2.5 rounded-full ${config.color}`}></div>
       <span className={`text-xs font-semibold uppercase tracking-wider ${config.textColor}`}>{config.label}</span>
     </div>
